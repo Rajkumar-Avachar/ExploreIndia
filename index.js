ca7a0ef4b7893ff0b5a6e99545717af7509fc918
Rajkumar <rajavachar59@gmail.com>
2024-11-18 21:21:00 +0530

    test db

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
 const heritage = require("./routes/heritage.js");
 const culture = require("./routes/culture.js");
+const dotenv = require('dotenv');
 
 
 app.set("view engine", "ejs");
@@ -15,9 +16,10 @@ app.engine("ejs", ejsMate);
 app.use(express.static("public"));
 app.use(express.static(path.join(__dirname, "public")));
 const UnescoSite = require("./models/schema.js");
+require('dotenv').config();
 
 async function main() {
-    const mongoURI = "mongodb+srv://rajavachar59:exploreindia@explore-india-db.57jx4.mongodb.net/exloreindiadb";
+    const mongoURI = process.env.MONGO_URI;
 
     try {
         await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
