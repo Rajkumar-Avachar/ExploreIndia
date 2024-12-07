ff4abaab6c841a4c674d11093babde8fb9a1b4e9
Rajkumar <rajavachar59@gmail.com>
2024-12-08 02:29:47 +0530

    update sessions

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.use(session({
     }),
     secret: 'thedarkknight',
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
     cookie: {
         maxAge: 7 * 24 * 60 * 60 * 1000,
         expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
